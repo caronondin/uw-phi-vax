44f258823319f93c09789825fa237e344e6b2bb1
Francisco Rios Casas <frc2@uw.edu>
2021-12-14 09:16:56 -0800

    Minor updates to script outlines

--- global_vac_index/aim_2/15_final_data_prep.R
+++ global_vac_index/aim_2/15_final_data_prep.R
@@ -1,1 +1,11 @@
-# Calculate geometric mean using each of the imputed datasets
+# Purpose: Final transformations of data before calculating index
+# Author: 
+# Date: Dec 13, 2021
+
+# Load the imputed data set
+
+# Save the first set
+
+# Transform the variables requiring transformatoon
+
+# calculate the vaccine index 
